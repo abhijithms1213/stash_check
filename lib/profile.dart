bcaacb5c88d100b4219f64e1d0dd36ccdc9a8dc5
Abhijith <abhijithms1213@gmail.com>
2025-01-16 13:54:26 +0530

    feat: :art: new profile pages added

--- lib/profile.dart
+++ lib/profile.dart
@@ -13,3 +13,29 @@ class _ScreenProfileState extends State<ScreenProfile> {
     return Container();
   }
 }
+class ScreenProfile2 extends StatefulWidget {
+  const ScreenProfile2({super.key});
+
+  @override
+  State<ScreenProfile2> createState() => _ScreenProfile2State();
+}
+
+class _ScreenProfile2State extends State<ScreenProfile2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class ScreenProfile3 extends StatefulWidget {
+  const ScreenProfile3({super.key});
+
+  @override
+  State<ScreenProfile> createState() => _ScreenProfile3State();
+}
+
+class _ScreenProfile3State extends State<ScreenProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
